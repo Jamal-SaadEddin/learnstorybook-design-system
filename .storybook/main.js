1e38211574345a6b29b9bc50078a9824cbbcecf6
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-07-15 14:00:03 +0300

    Test to maintain quality
    - Visual test appearance
    - Setup the test runner
    - Write an interaction test using the play function
    - Automate tests with the test runner
    - Run interaction tests in CI
    - Accessibility test

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,11 @@
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/react-vite',
